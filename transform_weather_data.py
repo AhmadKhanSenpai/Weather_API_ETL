ab3810d97706f6535287211fc38dde32874c1830
Muhammad <ahmad91309@gmail.com>
2026-07-26 17:44:35 +0500

    made some changes no longer need the small function i created earlier

--- transform_weather_data.py
+++ transform_weather_data.py
@@ -1,22 +1,43 @@
 from datetime import datetime as dt
 from datetime import timedelta as td
+import openmeteo_requests
+
+import pandas as pd
+import requests_cache
+from retry_requests import retry
 
 
-def get_date_range():
-    """
-    this will provide the date range i need for request 'start_date' and 'end_date parameters
-    it returns a tuple which contains start and end date
-    """
+# I used the list of dicts first but then i remember why flood memory with a list in case of large data we would need
+# a generator
+def parameter_builder(file_path):
+    df = pd.read_csv(file_path)
 
+    # calcualting date based on current date
     end_date = dt.now().strftime("%Y-%m-%d")
     diff = dt.now() - td(days=7)
     start_date = diff.strftime("%Y-%m-%d")
 
-    return (start_date, end_date)
+    for _, row in df.iterrows():
+        params = {
+            "latitude": row["latitude"],
+            "longitude": row["longitude"],
+            "hourly": ["temperature_2m", "relative_humidity_2m", "shortwave_radiation"],
+            "timezone": "auto",
+            "start_date": start_date,
+            "end_date": end_date,
+        }
+
+        yield row["site_code"], params
+
+
+def 
+
 
 
 # testing
 if __name__ == "__main__":
-    date = get_date_range()
-    print(f"end_date = {date[1]}")
-    print(f"start_date = {date[0]}")
+    result = parameter_builder("meta_data.csv")
+    for i, param in enumerate(result):
+        print(i, param)
+        if i > 10:
+            break
